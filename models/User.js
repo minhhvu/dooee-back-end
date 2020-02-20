a1d49e9582336ea9f74f43fda4833ff1d3095904
minhh <minhhaivuhd@gmail.com>
2020-02-20 00:18:08 -0500

    Create User model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+       type: String,
+       required: [true, 'Name is required'],
+       unique: true,
+       maxlength: [50, 'Name can not be more than 50 characters'],
+       minlength: [8, 'Name can not be less than 50 characters']
+    },
+    password: {
+        type: string,
+        required: [true, 'Password is required'],
+        maxlength: [50, 'Password can not be more than 50 characters'],
+        minlength: [8, 'Password can not be less than 50 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    roles: {
+        type: [String],
+        required: true,
+    },
+    actions: {
+        type: [String],
+    },
+    resources: {
+        type: [String],
+    },
+    operations: {
+        type: [String],
+    },
+    creatAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
