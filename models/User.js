1620cc261aa127c05ceb9f487a1afd09f38edacc
minhh <minhhaivuhd@gmail.com>
2020-02-24 17:06:08 -0500

    install packages

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'is required'],
-        unique: true
+        unique: true,
     },
     role: {
         type: String,
