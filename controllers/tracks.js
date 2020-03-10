88e554b1cdf9bc61aefa545f5467c958d8ce1954
minhh <minhhaivuhd@gmail.com>
2020-03-10 12:21:33 -0400

    create route: get/update/delete single track

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -10,3 +10,88 @@ const asyncHandler = require('../middlewares/async');
 exports.getTracks = [advancedResults(Track), asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
 })]
+
+
+//@desc Create a track
+//@route POST /api/v1/tracks
+//@access Private (only for admin)
+exports.createTrack = asyncHandler(async (req, res, next) => {
+    //Manage the fields
+    const reqBody = {...req.body};
+    ['owner', 'students'].forEach(field => delete reqBody[field]);
+
+    //Add owner information
+    reqBody.owner = req.user.id;
+
+    const track = await Track.create(reqBody);
+
+    res.status(201).json({
+        success: true,
+        data: track
+    })
+});
+
+//@desc get a track
+//@route GET /api/v1/tracks/:id
+//@access Private
+exports.getSingleTrack = asyncHandler(async (req, res, next) => {
+    const track = await Track.findById(req.params.id);
+
+    if (!track) {
+        return next(new ErrorResponse('Invalid Track Id'), 400);
+    }
+
+    res.status(201).json({
+        success: true,
+        data: track
+    })
+});
+
+//@desc Update a track
+//@route PUT /api/v1/tracks/:id
+//@access Private (only for owner)
+exports.updateTrack = asyncHandler(async (req, res, next) => {
+    let track = await Track.findById(req.params.id);
+
+    if (!track) {
+        return next(new ErrorResponse('Invalid Track Id'), 400);
+    }
+
+    if (track.owner.toString() !== req.user.id){
+        return next(new ErrorResponse('Not authorized to update course.', 401));
+    }
+
+    //Manage the field
+    const reqBody = {...req.body};
+    ['owner', 'students', 'courses', 'createAt', '_id'].forEach(field => delete reqBody[field]);
+
+    track = await Track.findByIdAndUpdate(req.params.id, reqBody, {new: true});
+
+    res.status(200).json({
+        success: true,
+        data: track
+    })
+});
+
+//@desc Delete a track
+//@route PUT /api/v1/tracks/:id
+//@access Private (only for owner)
+exports.deleteTrack = asyncHandler(async (req, res, next) => {
+    let track = await Track.findById(req.params.id);
+
+    if (!track) {
+        return next(new ErrorResponse('Invalid Track Id'), 400);
+    }
+
+    if (track.owner.toString() !== req.user.id){
+        return next(new ErrorResponse('Not authorized to update course.', 401));
+    }
+
+    track = await Track.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        data: 'Delete the track successfully.'
+    })
+});
+
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,19 @@
-const {getTracks} = require('../controllers/tracks');
+const {getTracks, createTrack, getSingleTrack, updateTrack, deleteTrack} = require('../controllers/tracks');
 const {protect, authorize} = require('../middlewares/auth');
 const express = require('express');
 const router = express.Router();
 
+router.use(protect);
+
 router
     .route('/')
-    .get(protect, getTracks);
+    .get(getTracks)
+    .post(authorize('admin'), createTrack);
+
+router
+    .route('/:id')
+    .get(getSingleTrack)
+    .put(updateTrack)
+    .delete(deleteTrack);
 
 module.exports = router;
